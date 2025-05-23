43d37d79c0b6bf6e84fb29c206e1fe77de60d8ec
dandl87 <dlorenzo.daniele@gmail.com>
2025-05-23 12:46:25 +0200

    test classes setup

--- src/test/java/com/delorenzo/Cinema/IntegrationTests.java
+++ src/test/java/com/delorenzo/Cinema/IntegrationTests.java
@@ -1,13 +1,9 @@
 package com.delorenzo.Cinema;
 
-import com.delorenzo.Cinema.conf.DateHolder;
-import com.delorenzo.Cinema.conf.StorageProperties;
 import com.delorenzo.Cinema.entity.Movie;
 import com.delorenzo.Cinema.entity.Room;
 import com.delorenzo.Cinema.repository.MovieRepository;
 import com.delorenzo.Cinema.repository.RoomRepository;
-import com.delorenzo.Cinema.repository.ScreeningRepository;
-import com.delorenzo.Cinema.service.*;
 import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +14,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
 
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -32,14 +27,6 @@ class IntegrationTests implements PostgreSQLContainerInitializer{
 	MovieRepository movieRepository;
 	@Autowired
 	RoomRepository roomRepository;
-	@Autowired
-	ScreeningRepository screeningRepository;
-	@Autowired
-	StorageProperties storageProperties;
-	@Autowired
-	ScreeningService screeningService;
-	@Autowired
-	DateHolder currentDay;
 
 
 	@BeforeAll
@@ -56,7 +43,7 @@ class IntegrationTests implements PostgreSQLContainerInitializer{
 
 	@Test
 	void contextLoads() {
-		assertTrue(postgresContainer.isRunning());
+		Assertions.assertTrue(postgresContainer.isRunning());
 	}
 
 	@Test
@@ -68,49 +55,6 @@ class IntegrationTests implements PostgreSQLContainerInitializer{
 		Assertions.assertEquals(2, imaxRooms.size());
 		Assertions.assertEquals(12, movies.size());
 	 }
-//
-//
-//	@DisplayName("Test di integrazione: inizializzazione")
-//	@Test
-//	void initBatchTest() throws Exception {
-//
-//		List<Scheduler> schedulers = schedulerInit();
-//		Optional<Scheduler> imaxScheduler = Utils.getSchedulerByName(schedulers,"imax");
-//		Optional<Scheduler> regularScheduler = Utils.getSchedulerByName(schedulers,"regular");
-//
-//		MovieService movieService = new MovieService(movieRepository);
-//		StorageService storageService = new FileSystemStorageService(storageProperties);
-//		storageService.deleteAll();
-//		storageService.init();
-//		MoviesFromExcelService moviesFromExcelService = new MoviesFromExcelService(storageProperties);
-//
-//		MainService mainService = new MainService(
-//				movieRepository,
-//				imaxScheduler.get(),
-//				regularScheduler.get(),
-//				movieService,
-//				screeningService,
-//				currentDay);
-//
-//		List<Screening> screenings = screeningRepository.findAll();
-//		Assertions.assertEquals(10, screenings.size());
-//	}
-//
-//
-//	private List<Scheduler> schedulerInit() throws Exception {
-//		List<Scheduler> schedulers = new ArrayList<>();
-//		Scheduler imaxScheduler = new Scheduler("imax",2);
-//		Scheduler regularScheduler = new Scheduler("regular",10);
-//		SchedulerDataLoader schedulerDataLoader = new SchedulerDataLoader(imaxScheduler,regularScheduler,roomRepository);
-//		schedulerDataLoader.run(new DefaultApplicationArguments());
-//		schedulers.add(imaxScheduler);
-//		schedulers.add(regularScheduler);
-//		return schedulers;
-//
-//	}
-
-
-
 
 
 }
